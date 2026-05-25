e1947849fee4662a43d646d4ba5ed2da493c661b
xmx-zzz <kato619.kdgo.student@gmail.com>
2026-05-25 15:10:51 +0900

    backendフォルダを作成

---
+++ backend/middleware/signup.php
@@ -0,0 +1,11 @@
+<h1>新規会員登録</h1>
+<form action="registere.php" method="post"><!-- 処理を行う宛先を指定 -->
+<div>
+    <label>
+        名前：
+        <input type="text" name="name" required>
+    </label>
+</div>
+<div>
+    <label>
+        
