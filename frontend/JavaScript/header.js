c8a520235310d8af780acb6c2a09c576f5b54fc1
miurayoshika <miu.happyflavor@gmail.com>
2026-07-13 11:39:08 +0900

    修正&カテゴリーボックス追加

--- frontend/JavaScript/header.js
+++ frontend/JavaScript/header.js
@@ -1,17 +1,36 @@
 const searchBox = document.getElementById("searchBox");
 const searchResult = document.getElementById("searchResult");
 
-searchBox.addEventListener("click", (e) => {
-  e.stopPropagation();
+const categoryBtn = document.getElementById("categoryBtn");
+const categoryMenu = document.getElementById("categoryMenu");
+const categoryArrow = document.getElementById("categoryArrow");
 
-  if (searchResult.style.display === "block") {
-    searchResult.style.display = "none";
-  } else {
-    searchResult.style.display = "block";
-  }
-});
+// 表示・非表示を切り替える関数
+function toggleMenu(button, menu, arrow = null, isLink = false) {
+  button.addEventListener("click", (e) => {
+    if (isLink) e.preventDefault();
+    e.stopPropagation();
+
+    const isOpen = menu.style.display === "block";
+
+    menu.style.display = isOpen ? "none" : "block";
+
+    if (arrow) {
+      arrow.classList.toggle("open", !isOpen);
+    }
+  });
+}
+
+// 検索メニュー
+toggleMenu(searchBox, searchResult);
+
+// カテゴリーメニュー
+toggleMenu(categoryBtn, categoryMenu, categoryArrow, true);
 
 // 外をクリックしたら閉じる
 document.addEventListener("click", () => {
   searchResult.style.display = "none";
+
+  categoryMenu.style.display = "none";
+  categoryArrow.classList.remove("open");
 });
